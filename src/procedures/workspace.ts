6cce03af3d80dfb20464bcd09562286534bd9913
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-10-08 20:27:28 +0530

    insert imageurl in db

--- src/procedures/workspace.ts
+++ src/procedures/workspace.ts
@@ -1,17 +1,26 @@
 import { db } from "@/db";
 import { workspace } from "@/db/schema";
+import { createWorkspaceSchema } from "@/schemas";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { createWorkspaceSchema } from "../schemas";
 
 export const workspaceRouter = createTRPCRouter({
   create: protectedProcedure
     .input(createWorkspaceSchema)
     .mutation(async ({ ctx, input }) => {
-      const { name } = input;
+      const { name, image } = input;
+
+      if (
+        image &&
+        !image.startsWith(
+          `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`
+        )
+      ) {
+        throw new Error("Invalid image source");
+      }
 
       const [createdWorkspace] = await db
         .insert(workspace)
-        .values({ name, userId: ctx.auth.user.id })
+        .values({ name, image, userId: ctx.auth.user.id })
         .returning();
 
       return createdWorkspace;
