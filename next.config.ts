d8a30b695de95085fc4848b6ff88e89b1e9d931d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-10-09 14:10:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
-import { FOLDER_PROJECT, FOLDER_WORKSPACE, TOP_FOLDER } from "@/constants";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typedRoutes: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        pathname: `/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/${TOP_FOLDER}/${FOLDER_WORKSPACE}/**`,
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: `/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/${TOP_FOLDER}/${FOLDER_PROJECT}/**`,
+        pathname: `/**`,
       },
     ],
   },
