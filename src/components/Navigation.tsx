952e9b135ad1c666022abe5e1379dad91939d059
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-10-10 11:45:19 +0530

    navigation bugs

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,61 +1,26 @@
 "use client";
 import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { routes } from "@/lib/routes";
 import { cn } from "@/lib/utils";
-import { RouteType } from "@/types";
-import { SettingsIcon, UserIcon } from "lucide-react";
 import type { Route } from "next";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-import {
-  GoCheckCircle,
-  GoCheckCircleFill,
-  GoHome,
-  GoHomeFill,
-} from "react-icons/go";
 
 const Navigation = () => {
   const pathname = usePathname();
   const workspaceId = useWorkspaceId();
 
-  const routes: RouteType = [
-    {
-      label: "Home",
-      href: "/",
-      icon: GoHome,
-      activeIcon: GoHomeFill,
-    },
-    {
-      label: "My Tasks",
-      href: "/tasks",
-      icon: GoCheckCircle,
-      activeIcon: GoCheckCircleFill,
-    },
-    {
-      label: "Members",
-      href: "/members",
-      icon: UserIcon,
-      activeIcon: UserIcon,
-    },
-    {
-      label: "Settings",
-      href: `/workspaces/${workspaceId}/settings`,
-      icon: SettingsIcon,
-      activeIcon: SettingsIcon,
-    },
-  ];
-
   return (
     <ul className="flex flex-col">
       {routes.map((r, idx) => {
-        const href = r.href.toString();
-        const isActive = pathname.endsWith(href);
+        const fullHref = `/workspaces/${workspaceId}${r.href}`;
+        const isActive = pathname === fullHref;
         const Icon = isActive ? r.activeIcon : r.icon;
 
         return (
           <Link
             key={idx.toString()}
-            href={r.href as Route}
+            href={fullHref as Route}
           >
             <div
               className={cn(
---
+++ src/lib/routes.ts
@@ -0,0 +1,36 @@
+import { RouteType } from "@/types";
+import { SettingsIcon, UserIcon } from "lucide-react";
+
+import {
+  GoCheckCircle,
+  GoCheckCircleFill,
+  GoHome,
+  GoHomeFill,
+} from "react-icons/go";
+
+export const routes: RouteType = [
+  {
+    label: "Home",
+    href: "",
+    icon: GoHome,
+    activeIcon: GoHomeFill,
+  },
+  {
+    label: "My Tasks",
+    href: "/tasks",
+    icon: GoCheckCircle,
+    activeIcon: GoCheckCircleFill,
+  },
+  {
+    label: "Members",
+    href: "/members",
+    icon: UserIcon,
+    activeIcon: UserIcon,
+  },
+  {
+    label: "Settings",
+    href: `/settings`,
+    icon: SettingsIcon,
+    activeIcon: SettingsIcon,
+  },
+];
