b0abb52dd68e6553531d390fe524c50747e38e82
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-10-14 19:30:48 +0530

    projectIdPage

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -0,0 +1,49 @@
+import { ErrorBoundaryWrapper } from "@/components/fallbacks/ErrorBoundaryWrapper";
+import PageLoading from "@/components/fallbacks/PageLoading";
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+import ProjectIdView from "@/views/projects/ProjectIdView";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+
+interface Props {
+  params: Promise<{ workspaceId: string; projectId: string }>;
+}
+
+const ProjectIdPage = async ({ params }: Props) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  } else if (!session.user.emailVerified) {
+    redirect(`/email-verification?email=${session.user.email}`);
+  }
+
+  const workspaceId = (await params).workspaceId;
+  const projectId = (await params).projectId;
+
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(
+    trpc.project.getMany.queryOptions({ workspaceId })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<PageLoading />}>
+        <ErrorBoundaryWrapper>
+          <ProjectIdView
+            workspaceId={workspaceId}
+            projectId={projectId}
+          />
+        </ErrorBoundaryWrapper>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default ProjectIdPage;
---
+++ src/views/projects/ProjectIdView.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  workspaceId: string;
+  projectId: string;
+}
+
+const ProjectIdView = ({ projectId, workspaceId }: Props) => {
+  return <div>ProjectIdView</div>;
+};
+
+export default ProjectIdView;
