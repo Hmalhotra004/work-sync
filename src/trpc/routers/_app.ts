49f7d83fdc9905da52561d16c087693bca516c61
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-10-15 13:57:47 +0530

    router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { createTRPCRouter } from "../init";
 import { cloudinaryRouter } from "@/procedures/cloudinary";
 import { memberRouter } from "@/procedures/member";
 import { projectRouter } from "@/procedures/project";
+import { taskRouter } from "@/procedures/task";
 import { workspaceRouter } from "@/procedures/workspace";
 
 export const appRouter = createTRPCRouter({
   workspace: workspaceRouter,
   project: projectRouter,
+  task: taskRouter,
   member: memberRouter,
   cloudinary: cloudinaryRouter,
 });
