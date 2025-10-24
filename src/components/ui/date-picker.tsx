0cb80bbbb18a8f81ad24cf9c3028e38cc255e782
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-10-24 15:41:49 +0530

    Create date-picker.tsx

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Button } from "./button";
+import { Calendar } from "./calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+
+interface Props {
+  value: string | undefined;
+  onChange: (date: string) => void;
+  className?: ClassValue;
+  placeholder?: string;
+}
+
+const DatePicker = ({ onChange, value, className, placeholder }: Props) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="lg"
+          className={cn(
+            "w-full justify-start text-left font-normal px-3",
+            !value && "text-muted-foreground",
+            className
+          )}
+        >
+          <CalendarIcon className="mr-2 size-4" />
+          {value ? format(value, "PPP") : <span>{placeholder}</span>}
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={value ? new Date(value) : undefined}
+          onSelect={(date) => {
+            if (date) onChange(date?.toISOString());
+          }}
+          autoFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DatePicker;
