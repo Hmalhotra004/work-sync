af23c8495041071367ad79491a4d0497a6b0343d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-11-10 17:02:08 +0530

    Create ProfileFom.tsx

---
+++ src/components/form/ProfileFom.tsx
@@ -0,0 +1,280 @@
+"use client";
+
+import FormInput from "@/components/form/FormInput";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import DottedSeparator from "@/components/ui/dotted-separator";
+import { cn } from "@/lib/utils";
+import { updateUserSchema } from "@/schemas/profile/schema";
+import { useTRPC } from "@/trpc/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { User as UserType } from "better-auth";
+import { ImageIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import type z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+interface Props {
+  user: UserType;
+}
+
+const ProfileForm = ({ user }: Props) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const [isPending, setIsPending] = useState(false);
+  const [preview, setPreview] = useState(user.image ?? null);
+  const [file, setFile] = useState<File | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const form = useForm<z.infer<typeof updateUserSchema>>({
+    resolver: zodResolver(updateUserSchema),
+    defaultValues: {
+      name: user.name ?? "",
+      image: user.image ?? undefined,
+    },
+  });
+
+  const deleteProfileImage = useMutation(
+    trpc.profile.deleteProfileImage.mutationOptions()
+  );
+
+  const getSignature = useMutation(
+    trpc.cloudinary.getUploadSignature.mutationOptions()
+  );
+
+  async function uploadImageToCloudinary(file: File) {
+    const { cloudName, apiKey, timestamp, signature, folder } =
+      await getSignature.mutateAsync({ folder: "Workspace" });
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("api_key", apiKey);
+    formData.append("timestamp", timestamp.toString());
+    formData.append("signature", signature);
+    formData.append("folder", folder);
+
+    const uploadRes = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      { method: "POST", body: formData }
+    );
+
+    if (!uploadRes.ok) throw new Error("Image upload failed");
+    const uploadData = await uploadRes.json();
+    return uploadData.secure_url as string;
+  }
+
+  function handleImageSelect(e: React.ChangeEvent<HTMLInputElement>) {
+    const selected = e.target.files?.[0];
+    if (!selected) return;
+
+    if (!selected.type.startsWith("image/")) {
+      toast.error("Only image files are allowed");
+      return;
+    }
+
+    if (selected.size > 5 * 1024 * 1024) {
+      toast.error("Image size must be less than 5MB");
+      return;
+    }
+
+    setFile(selected);
+    setPreview(URL.createObjectURL(selected));
+    form.setValue("image", URL.createObjectURL(selected));
+  }
+
+  function clearImg() {
+    setFile(null);
+    setPreview(null);
+    form.setValue("image", undefined);
+    if (inputRef.current) inputRef.current.value = "";
+  }
+
+  function resetForm() {
+    form.reset();
+    setPreview(user.image ?? null);
+    setFile(null);
+    if (inputRef.current) inputRef.current.value = "";
+  }
+
+  const onSubmit = async (values: z.infer<typeof updateUserSchema>) => {
+    try {
+      setIsPending(true);
+      let imageUrl = values.image;
+
+      if (file) {
+        if (user.image) {
+          await deleteProfileImage.mutateAsync();
+        }
+
+        imageUrl = await uploadImageToCloudinary(file);
+      } else if (!file) {
+        if (preview) {
+          imageUrl = preview;
+        } else {
+          await deleteProfileImage.mutateAsync();
+          imageUrl = undefined;
+        }
+      }
+
+      resetForm();
+    } catch (err) {
+      if (err instanceof Error) {
+        toast.error(err.message);
+      } else {
+        toast.error("An unknown error occurred");
+      }
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-5"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Profile Name</FormLabel>
+              <FormControl>
+                <FormInput
+                  type="text"
+                  placeholder="Enter profile name"
+                  autoComplete="off"
+                  disabled={isPending}
+                  field={field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          name="image"
+          control={form.control}
+          render={() => (
+            <div className="flex flex-col gap-y-2">
+              <div className="flex items-center gap-x-5">
+                {preview ? (
+                  <div className="relative size-[72px] rounded-md">
+                    <Image
+                      src={preview}
+                      alt="Profile Image"
+                      fill
+                      className="object-cover rounded-md"
+                    />
+                  </div>
+                ) : (
+                  <Avatar className="size-[72px]">
+                    <AvatarFallback>
+                      <ImageIcon className="size-[36px] text-foreground-400" />
+                    </AvatarFallback>
+                  </Avatar>
+                )}
+
+                <FormControl>
+                  <div>
+                    <p className="text-sm font-medium">Profile Image</p>
+                    <p className="text-sm text-muted-foreground">
+                      JPG, PNG, JPEG or SVG (max 5MB)
+                    </p>
+                    <input
+                      ref={inputRef}
+                      type="file"
+                      accept=".jpg, .jpeg, .png, .svg"
+                      className="hidden"
+                      disabled={isPending}
+                      onChange={handleImageSelect}
+                    />
+                    {preview ? (
+                      <Button
+                        type="button"
+                        variant="destructive"
+                        size="xs"
+                        className="mt-2 w-fit"
+                        onClick={clearImg}
+                        disabled={isPending}
+                      >
+                        Remove Image
+                      </Button>
+                    ) : (
+                      <Button
+                        type="button"
+                        variant="teritary"
+                        size="xs"
+                        className="mt-2 w-fit"
+                        onClick={() => inputRef.current?.click()}
+                        disabled={isPending}
+                      >
+                        Upload Image
+                      </Button>
+                    )}
+                  </div>
+                </FormControl>
+              </div>
+              <FormMessage />
+            </div>
+          )}
+        />
+
+        <DottedSeparator
+          className={cn(
+            "transition",
+            !form.formState.isDirty && !file && "hidden"
+          )}
+        />
+
+        <div
+          className={cn(
+            "flex items-center justify-between",
+            !form.formState.isDirty && !file && "hidden"
+          )}
+        >
+          <Button
+            type="button"
+            variant="secondary"
+            size="lg"
+            onClick={() => resetForm()}
+            disabled={isPending}
+            className={cn(
+              "transition-opacity",
+              !form.formState.isDirty && !file && "hidden"
+            )}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            type="submit"
+            size="lg"
+            disabled={isPending}
+          >
+            Update Profile
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
