66b055c33ac1c203b10147760c384779a74b9adf
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-11-10 16:59:54 +0530

    Add profile image deletion endpoint
    Introduces a new protected mutation in profileRouter to delete a user's profile image from Cloudinary and update the database. Registers profileRouter in the main appRouter for TRPC.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "../init";
 
 import { cloudinaryRouter } from "@/procedures/cloudinary";
 import { memberRouter } from "@/procedures/member";
+import { profileRouter } from "@/procedures/profile";
 import { projectRouter } from "@/procedures/project";
 import { taskRouter } from "@/procedures/task";
 import { workspaceRouter } from "@/procedures/workspace";
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   project: projectRouter,
   task: taskRouter,
   member: memberRouter,
+  profile: profileRouter,
   cloudinary: cloudinaryRouter,
 });
 
