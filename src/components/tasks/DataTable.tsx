b633c0a8ceeca2fc7daee5bc32b74cb566d92ad1
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-11-12 16:47:02 +0530

    Comment out pagination controls in DataTable
    The pagination button controls in the DataTable component have been commented out, possibly to temporarily disable pagination UI while retaining the code for future use.

--- src/components/tasks/DataTable.tsx
+++ src/components/tasks/DataTable.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 import {
@@ -113,7 +112,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      {/* <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="secondary"
           size="sm"
@@ -130,7 +129,7 @@ export function DataTable<TData, TValue>({
         >
           Next
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 }
